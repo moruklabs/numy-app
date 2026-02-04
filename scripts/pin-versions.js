c1dbfe3aa7a70ba2285f136879b67b27f3eb82ae
Moruk <dev@moruk.ai>
2026-02-04 03:14:37 +0100

    chore(scripts): update pin-versions script

--- scripts/pin-versions.js
+++ scripts/pin-versions.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const pkg = require('../package.json');
+const fs = require("fs");
+const path = require("path");
+const pkg = require("../package.json");
 
 const getVersion = (dep) => {
   try {
     // Look in node_modules in the root
-    const p = path.join(__dirname, '..', 'node_modules', dep, 'package.json');
+    const p = path.join(__dirname, "..", "node_modules", dep, "package.json");
     if (fs.existsSync(p)) {
-      return JSON.parse(fs.readFileSync(p, 'utf8')).version;
+      return JSON.parse(fs.readFileSync(p, "utf8")).version;
     }
-  } catch (e) { }
+  } catch (e) {}
   return null;
 };
 
-['dependencies', 'devDependencies'].forEach(type => {
+["dependencies", "devDependencies"].forEach((type) => {
   if (!pkg[type]) return;
   for (const dep of Object.keys(pkg[type])) {
     const v = getVersion(dep);
